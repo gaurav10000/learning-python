7f22a8c3bda6112279b6116922f47283a9dab87b
gaurav10000 <gaurav.saraswat_cs21@gla.ac.in>
2022-04-27 19:15:15 +0530

    removing some lines

--- reversenum.py
+++ reversenum.py
@@ -1,15 +1,10 @@
-from  timeit import default_timer as timer
+
 def rev(n):
      r = 0
      while(n>=1):
          temp = n%10
          r = r*10 + temp
          n = n//10
-        
      return(r)
-    
 a = int(input("Enter a number: "))
-start = timer()
 print(rev(a))
-stop = timer()
-print(stop-start)
