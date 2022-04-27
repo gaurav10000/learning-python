9c9916b0a075e7ba0c2d362ae0d3f5f77b9ce3e4
gaurav10000 <gaurav.saraswat_cs21@gla.ac.in>
2022-04-27 23:36:36 +0530

    new programs

--- tryhere.py
+++ tryhere.py
@@ -1,6 +1,21 @@
-import random
-
-
-s = 'hey how are you'
-ltr = s.split( )
-print(random.choice(ltr))
+from math import sqrt
+ 
+def isPrime(n):
+ 
+    # Corner case
+    if (n <= 1):
+        return False
+ 
+    # Check from 2 to sqrt(n)
+    for i in range(2, int(sqrt(n))+1):
+        if (n % i == 0):
+            return False
+ 
+    return True
+ 
+ 
+# Driver Code
+if isPrime(1):
+    print("true")
+else:
+    print("false")
