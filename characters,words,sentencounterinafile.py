92fd67479cfed87e24554514e7c95bf28747c02d
gaurav10000 <gaurav.saraswat_cs21@gla.ac.in>
2022-05-11 21:50:27 +0530

    new codes

--- characters,words,sentencounterinafile.py
+++ characters,words,sentencounterinafile.py
@@ -2,12 +2,12 @@ from asyncore import read
 g= open("abc.txt","r")
 s = g.read()
 k = 0
-sentences = s.count('\n')
+lines = s.count('\n')
 words = s.count(' ')
 for i in s: 
     k = k+1
-print('sentences in file are',sentences+1)
-print('words in file are',sentences + words + 1)
-print('words in file are',k - sentences)
+print('sentences in file are',lines+1)
+print('words in file are',lines + words + 1)
+print('words in file are',k - lines)
 g.close()
 
